85242be4acd463234c3c6fbeccbb6b682dac92ae
GoHyunkyu <gusrb0296@naver.com>
2023-11-19 14:38:03 +0900

    Add 3, 4 problem
    3, 4번 문제 해결

---
+++ CodeKata/CodeKata/03.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    internal class _3
+    {
+        static public int Solution(int num1, int num2) 
+        {
+            int answer = 0;
+
+            answer = num1 / num2;
+            
+            return answer;
+        }
+    }
+}
