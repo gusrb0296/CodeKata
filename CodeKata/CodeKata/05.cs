7fe88ccef8d3f0825655efa88eea0ab25bc223b9
GoHyunkyu <gusrb0296@naver.com>
2023-11-20 09:23:00 +0900

    Add 05, 06 soulution
    5번, 6번 문제 풀이

---
+++ CodeKata/CodeKata/05.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata
+{
+    internal class _05
+    {
+        public int solution(int num1, int num2)
+        {
+            int answer;
+
+            answer = num1 == num2 ? 1 : -1;
+
+            return answer;
+        }
+    }
+}
--- CodeKata/CodeKata/Program.cs
+++ CodeKata/CodeKata/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             int answer = 0;
+
+            _05 _05 = new _05();
+
+            answer = _05.solution(1, 1);
+            Console.WriteLine(answer);
         }
     }
 }
