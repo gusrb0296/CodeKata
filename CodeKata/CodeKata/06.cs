19bfe0891354f45549f4057f38717a58f053bcc9
GoHyunkyu <gusrb0296@naver.com>
2023-11-20 09:26:28 +0900

    Update 06.cs

--- CodeKata/CodeKata/06.cs
+++ CodeKata/CodeKata/06.cs
@@ -8,5 +8,13 @@ namespace CodeKata
 {
     internal class _06
     {
+        public int solution(int num1, int num2)
+        {
+            int answer = -1;
+
+            answer = num1 + num2;
+
+            return answer;
+        }
     }
 }
