1844a43519169e0005c1b4feed1c8bdcc060b10b
GoHyunkyu <gusrb0296@naver.com>
2024-01-10 10:12:43 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.70 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
@@ -1,28 +1,20 @@
 using System;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
-
 public class Solution {
     public int solution(string s) {
-        Dictionary<string, string> dicNum= new Dictionary<string, string>{
-            {"one", "1"},
-            {"two", "2"},
-            {"three", "3"},
-            {"four", "4"},
-            {"five", "5"},
-            {"six", "6"},
-            {"seven", "7"},
-            {"eight", "8"},
-            {"nine", "9"},
-            {"zero", "0"}
-        };
+        int answer = 0;
         
-        string str = s;
-        //foreach(var replaceAnswer in dicNum){ var 사용 가능
-        foreach(KeyValuePair<string, string> replaceAnswer in dicNum){
-            str = Regex.Replace(str, replaceAnswer.Key, replaceAnswer.Value);
-        }
-        int answer = int.Parse(str);
+        s = s.Replace("zero", "0");
+        s = s.Replace("one", "1");
+        s = s.Replace("two", "2");
+        s = s.Replace("three", "3");
+        s = s.Replace("four", "4");
+        s = s.Replace("five", "5");
+        s = s.Replace("six", "6");
+        s = s.Replace("seven", "7");
+        s = s.Replace("eight", "8");
+        s = s.Replace("nine", "9");
+        
+        answer = int.Parse(s);
         return answer;
     }
 }
