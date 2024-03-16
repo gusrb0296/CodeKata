f49c8245e4334bf13195fddfba2b8ea293f51552
GoHyunkyu <gusrb0296@naver.com>
2024-03-16 22:30:44 +0900

    [Bronze V] Title: A+B, Time: 64 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -0,0 +1,12 @@
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] num = Console.ReadLine().Split();
+                           
+        int a = int.Parse(num[0]);
+        int b = int.Parse(num[1]);
+        Console.WriteLine(a + b);
+    }    
+}
