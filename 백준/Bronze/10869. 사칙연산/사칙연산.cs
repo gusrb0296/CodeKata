0b2c91418c67acd723d19ac94aa54e26203a346e
GoHyunkyu <gusrb0296@naver.com>
2024-03-17 22:08:07 +0900

    [Bronze V] Title: 사칙연산, Time: 68 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,13 @@
+class program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split();
+        
+        Console.WriteLine(int.Parse(input[0]) + int.Parse(input[1]));
+        Console.WriteLine(int.Parse(input[0]) - int.Parse(input[1]));
+        Console.WriteLine(int.Parse(input[0]) * int.Parse(input[1]));
+        Console.WriteLine(int.Parse(input[0]) / int.Parse(input[1]));
+        Console.WriteLine(int.Parse(input[0]) % int.Parse(input[1]));
+    }
+}
