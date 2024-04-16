076ef5c0b19a8aab6b15d8f752a4f36509fca40c
GoHyunkyu <gusrb0296@naver.com>
2024-04-16 23:58:16 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 288 ms, Memory: 6580 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cs
@@ -0,0 +1,16 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count = int.Parse(Console.ReadLine());
+        
+        for(int i = 0; i < count; i ++)
+        {
+            for(int j = 0; j <= i; j ++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
